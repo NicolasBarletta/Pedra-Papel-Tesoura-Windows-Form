4a901785da7fa10495ace9afa7a876ae2b05739d
Nicolas C L Barletta <58794347+Nickgw25@users.noreply.github.com>
2021-03-09 20:50:31 -0300

    Add files via upload

---
+++ PedraPapelTesouraWF/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace PedraPapelTesouraWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button4.Font = new System.Drawing.Font("Snap ITC", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(182, 108);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(326, 105);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "PEDRA";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button5.Font = new System.Drawing.Font("Snap ITC", 24.75F);
+            this.button5.Location = new System.Drawing.Point(182, 244);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(326, 105);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "PAPEL";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button6.Font = new System.Drawing.Font("Snap ITC", 24.75F);
+            this.button6.Location = new System.Drawing.Point(182, 380);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(326, 105);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "TESOURA";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Snap ITC", 24.75F);
+            this.label7.Location = new System.Drawing.Point(964, 100);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(257, 43);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Sua Escolha:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Snap ITC", 24.75F);
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(1002, 170);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 43);
+            this.label8.TabIndex = 4;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Snap ITC", 24.75F);
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label9.Location = new System.Drawing.Point(1019, 301);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 43);
+            this.label9.TabIndex = 6;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Snap ITC", 24.75F);
+            this.label10.Location = new System.Drawing.Point(873, 244);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(459, 43);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "Escolha do Computador:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Snap ITC", 24.75F);
+            this.label11.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label11.Location = new System.Drawing.Point(993, 451);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 43);
+            this.label11.TabIndex = 8;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Snap ITC", 24.75F);
+            this.label12.Location = new System.Drawing.Point(993, 401);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(217, 43);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Resultado:";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1426, 617);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ PedraPapelTesouraWF/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PedraPapelTesouraWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+   
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int pc = r.Next(0, 3);
+
+            if (pc == 0)
+            {
+                label8.Text = ("PEDRA");
+                label9.Text = ("PEDRA");
+                label11.Text = ("EMPATE!");
+
+            }
+            else if (pc == 1)
+            {
+                label8.Text = ("PEDRA");
+                label9.Text = ("PAPEL");
+                label11.Text = ("DERROTA!");
+
+            }
+            else if (pc == 2)
+            {
+                label8.Text = ("PEDRA");
+                label9.Text = ("TESOURA");
+                label11.Text = ("VITÓRIA!");
+
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int pc = r.Next(0, 3);
+
+            if (pc == 0)
+            {
+                label8.Text = ("PAPEL");
+                label9.Text = ("PEDRA");
+                
+                label11.Text = ("VITÓRIA!");
+                
+            }
+            else if (pc == 1)
+            {
+                label8.Text = ("PAPEL");
+                label9.Text = ("PAPEL");
+                label11.Text = ("EMPATE!");
+
+            }
+            else if (pc == 2)
+            {
+                label8.Text = ("PAPEL");
+                label9.Text = ("TESOURA");
+                label11.Text = ("DERROTA!");
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int pc = r.Next(0, 3);
+
+            if (pc == 0)
+            {
+                label8.Text = ("TESOURA");
+                label9.Text = ("PEDRA");
+                label11.Text = ("DERROTA!");
+
+            }
+            else if (pc == 1)
+            {
+                label8.Text = ("TESOURA");
+                label9.Text = ("PAPEL");
+                label11.Text = ("VITÓRIA!");
+
+            }
+            else if (pc == 2)
+            {
+                label8.Text = ("TESOURA");
+                label9.Text = ("TESOURA");
+                label11.Text = ("EMPATE!");
+
+            }
+        }
+    }
+    }
+
